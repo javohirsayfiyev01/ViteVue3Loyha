e9eb0c268970dfde27fc1ef2569dc2f4880ed7a3
javohirsayfiyev01 <javohirsayfiyev01@gmail.com>
2024-09-18 16:39:02 +0500

    first commit

---
+++ vitemarketplace/src/App.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div>
+Salom TypeScript, Vite va Vue
+<header>
+  <img src="./assets/vue.svg" alt="">
+  <h1>Pinya Tasks</h1>
+</header>
+
+<!-- task list -->
+ <div class="task-list">
+  <div v-for="task in taskStore.tasks" :key="task">
+    <p>{{ task.title}}</p>
+  </div>
+ </div>
+  </div>
+</template>
+<script>
+import {useTaskStore} from './stores/TaskStore'
+
+export default {
+
+  setup () {
+    debugger
+   const taskStore = useTaskStore()
+
+   return {taskStore}
+  }
+}
+</script>
+
+<style>
+</style>
