cb58b7fddef65697c897112188f6be264f3d33dc
javohirsayfiyev01 <javohirsayfiyev01@gmail.com>
2024-09-20 11:46:42 +0500

    getters va actions ni o'rganish

--- vitemarketplace/src/App.vue
+++ vitemarketplace/src/App.vue
@@ -5,28 +5,34 @@ Salom TypeScript, Vite va Vue
 <header>
   <img src="./assets/vue.svg" alt="">
   <h1>Pinya Tasks</h1>
+  <p></p>
 </header>
 
 <!-- task list -->
  <div class="task-list">
-  <div v-for="task in taskStore.tasks" :key="task">
-    <p>{{ task.title}}</p>
+  <p>all tasks</p>
+  <div v-for="task in taskStore.tasks" :key="task"> <!-- `taskStore.tasks` bu Array qaytaradi -->
+    <TaskDetails :task="task"/>
   </div>
  </div>
+ <div class="task-list">
+  <p>fav tasks</p>
+  <div v-for="task in taskStore.favs" :key="task">
+    <TaskDetails :task="task"/>
+  </div>
+
+  <p>{{ taskStore.doubleCount }}</p>
+  <button @click="taskStore.incrementCount">bos</button>
+
+</div>
   </div>
 </template>
-<script>
+<script setup>
+import TaskDetails from './components/TaskDetails.vue'
 import {useTaskStore} from './stores/TaskStore'
 
-export default {
+const taskStore = useTaskStore() /// `taskStore` bu OBJ.
 
-  setup () {
-    debugger
-   const taskStore = useTaskStore()
-
-   return {taskStore}
-  }
-}
 </script>
 
 <style>
--- vitemarketplace/src/stores/TaskStore.js
+++ vitemarketplace/src/stores/TaskStore.js
@@ -3,9 +3,27 @@ import {defineStore} from 'pinia'
 export const useTaskStore = defineStore('taskStore', {
    state: () => ({
     tasks: [
-
       {id: 1, title: "buy some milk", isFav: false},
       {id: 2, title: " some milk", isFav: true},
-    ]
-   })
+    ],
+    count: 0
+   }),
+   /// `getters`
+
+
+    doubleCount(state){
+      debugger
+      return state.count * 2
+
+
+   },
+   actions:{
+    incrementCount(){
+      // debugger
+      console.log(this.count);
+      this.count++
+     console.log(this.count);
+
+    }
+   }
 })
